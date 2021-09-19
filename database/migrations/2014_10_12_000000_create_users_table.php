6057a84806dec4b563c2a12169cb05789574bfa9
Cesar Rivera <mondragonrivera2013@gmail.com>
2021-09-19 14:16:36 -0600

    Add plantilla Cork and migrations y models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class CreateUsersTable extends Migration
 {
@@ -16,9 +17,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone',10)->nullable();
             $table->string('email')->unique();
+            $table->enum('profile',['ADMIN','EMPLOYEE'])->default('ADMIN');
+            $table->enum('status',['ACTIVE','LOCKED'])->default('ACTIVE');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image',50)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_09_17_023403_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->text('address',500);
+            $table->string('phone',11);
+            $table->string('NIT',14)->nullable();
+            $table->string('NRC',10)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
